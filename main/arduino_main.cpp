1be08a5edfa95ff92a4f894c740a7f489ecf9e19
Akshay Unnikrishnan <akshay.unni@utexas.edu>
2024-10-17 21:41:06 -0500

    Added controller code

--- main/arduino_main.cpp
+++ main/arduino_main.cpp
@@ -32,6 +32,8 @@ limitations under the License.
 
 #define IN1 12
 #define IN2 14
+#define BIN1 27
+#define BIN2 26
 
 Servo servo;
 
@@ -60,43 +62,105 @@ void onDisconnectedGamepad(GamepadPtr gp) {
     }
 }
 
+
 void setup() {
     BP32.setup(&onConnectedGamepad, &onDisconnectedGamepad);
     BP32.forgetBluetoothKeys();
+
  
-    servo.attach(15);
+    //servo.attach(15);
 
     // motor controller outputs
     pinMode(IN1, OUTPUT);
     pinMode(IN2, OUTPUT);
+    pinMode(BIN1, OUTPUT);
+    pinMode(BIN2, OUTPUT);
+    pinMode(2, OUTPUT);
+    
    
     Serial.begin(115200);
 }
 
 void loop() {
     BP32.update();
+
+    Serial.println("Looping");
+    // digitalWrite(IN1, HIGH);
+    // digitalWrite(IN2, LOW);
+    // digitalWrite(BIN1, LOW);
+    // digitalWrite(BIN2, HIGH);
+
+    //     delay(2000);
+
+    // Serial.println(" DC motor stop");
+    //     digitalWrite(IN1, LOW);
+    //     digitalWrite(IN2, LOW);
+    //     digitalWrite(BIN1, LOW);
+    //     digitalWrite(BIN2, LOW);
+
+
+
+
     for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
         GamepadPtr controller = myGamepads[i];
         if (controller && controller->isConnected()) {
+            
+            Serial.println("Connected");
+            digitalWrite(2, HIGH);
+            delay(2000);
+            digitalWrite(2, LOW);
            
-            if (controller->l1() == 1) {
-                Serial.print("Servo move");
-                servo.write(1000);
-            }
-            if (controller->l1() == 0) {
-                Serial.print("Servo stop");
-                servo.write(1500);
+            // if (controller->l1() == 1) {
+            //     Serial.print("Servo move");
+            //     servo.write(1000);
+            // }
+            // if (controller->l1() == 0) {
+            //     Serial.print("Servo stop");
+            //     servo.write(1500);
+            // }
+
+            //Move forward
+            if(controller->axisY() < 0) { // negative y is upward on stick
+                Serial.println(" DC motor move");
+                digitalWrite(IN1, HIGH);
+                digitalWrite(IN2, LOW);
+                digitalWrite(BIN1, LOW);
+                digitalWrite(BIN2, HIGH);
             }
 
-            if(controller->axisRY() > 0) { // negative y is upward on stick
+            //Move backward
+            if(controller->axisY() > 0) {
                 Serial.println(" DC motor move");
                 digitalWrite(IN1, LOW);
                 digitalWrite(IN2, HIGH);
+                digitalWrite(BIN1, HIGH);
+                digitalWrite(BIN2, LOW);
             }
-            if(controller->axisRY() == 0) { // stop motor 1
-                Serial.println(" DC motor stop");
+
+            //Stop moving
+            if(controller->axisY() == 0) { // negative y is upward on stick
+                Serial.println(" DC motor move");
                 digitalWrite(IN1, LOW);
                 digitalWrite(IN2, LOW);
+                digitalWrite(BIN1, LOW);
+                digitalWrite(BIN2, LOW);
+            }
+
+            // if(controller->axisRX() > 0) { 
+            //     Serial.println(" DC motor move");
+            //     digitalWrite(IN1, LOW);
+            //     digitalWrite(IN2, HIGH);
+            // }
+            // if(controller->axisRX() < 0) { // stop motor 1
+            //     Serial.println(" DC motor stop");
+            //     digitalWrite(IN1, LOW);
+            //     digitalWrite(IN2, LOW);
+            // }
+
+            // if(controller->axisRX() = 0) { // stop motor 1
+            //     Serial.println(" DC motor stop");
+            //     digitalWrite(IN1, LOW);
+            //     digitalWrite(IN2, LOW);
             }
 
             // PHYSICAL BUTTON A
@@ -104,7 +168,6 @@ void loop() {
                 Serial.println("button a pressed");
             }
 
-        }
-        vTaskDelay(1);
     }
+    vTaskDelay(1);
 }
