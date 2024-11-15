f96ed917cef20ffd89077725b7daa84ffb97e8da
Akshay Unnikrishnan <akshay.unni@utexas.edu>
2024-11-15 02:33:16 -0600

    Updated stuff

--- main/arduino_main.cpp
+++ main/arduino_main.cpp
@@ -45,6 +45,10 @@ limitations under the License.
 #define I2C_SLC 22 //GPIO Pin 
 #define I2C_FREQ 100000
 
+// LED Vars
+#define LED1 25
+#define LED2 15
+
 // OBJECTS //
 // Servo
 Servo servo;
@@ -57,7 +61,10 @@ TwoWire I2C_0 = TwoWire(0);
 APDS9960 sensor = APDS9960(I2C_0, APDS9960_INT);
 
 // IR Sensor (Distance Sensor)
-ESP32SharpIR IRSensorName(ESP32SharpIR::GP2Y0A21YK0F, 25);
+ESP32SharpIR frontSensor(ESP32SharpIR::GP2Y0A21YK0F, 36); //front sensor
+ESP32SharpIR sideSensor1(ESP32SharpIR::GP2Y0A21YK0F, 39); //side sensor
+ESP32SharpIR sideSensor2(ESP32SharpIR::GP2Y0A21YK0F, 34); // side sensor
+
 
 // Line Follower
 QTRSensors qtr;
@@ -93,7 +100,7 @@ void setup() {
     BP32.forgetBluetoothKeys();
 
     // Servo Setup
-    //servo.attach(15);
+    servo.attach(4);
 
     // Motor Setup
     pinMode(IN1, OUTPUT);
@@ -103,6 +110,8 @@ void setup() {
 
     // ESP32 Light Setup
     pinMode(2, OUTPUT);
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);
 
     // Color Sensor Setup
     I2C_0.begin(I2C_SDA, I2C_SLC, I2C_FREQ);
@@ -110,11 +119,13 @@ void setup() {
     sensor.begin();
 
     // IR Sensor (Distance Sensor) Setup
-    IRSensorName.setFilterRate(1.0f);
+    frontSensor.setFilterRate(1.0f);
+    sideSensor1.setFilterRate(1.0f);
+    sideSensor2.setFilterRate(1.0f);
 
     // Line Follower Setup
     qtr.setTypeAnalog(); // or setTypeAnalog()
-    qtr.setSensorPins((const uint8_t[]) {33, 32}, 2); // pin numbers go in the curly brackets {}, and number of pins goes after
+    qtr.setSensorPins((const uint8_t[]) {2, 0}, 2); // pin numbers go in the curly brackets {}, and number of pins goes after
     // calibration sequence
     for (uint8_t i = 0; i < 250; i++) { 
         Serial.println("calibrating");
@@ -127,7 +138,7 @@ void setup() {
 }
 
 void motorTest(){
-    Serial.println("Looping");
+    Serial.println("Forward");
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
     digitalWrite(BIN1, LOW);
@@ -140,6 +151,8 @@ void motorTest(){
     digitalWrite(IN2, LOW);
     digitalWrite(BIN1, LOW);
     digitalWrite(BIN2, LOW);
+
+    delay(2000);
 }
 
 void servoTest(){
@@ -177,7 +190,12 @@ void colorSenseTest(){
 }
 
 void irSensorTest() {
-    Serial.println(IRSensorName.getDistanceFloat()); 
+    Serial.print("Front Sensor: ");
+    Serial.println(frontSensor.getDistanceFloat()); 
+    Serial.print("Right Sensor: ");
+    Serial.println(sideSensor1.getDistanceFloat()); 
+    Serial.print("Left Sensor: ");
+    Serial.println(sideSensor2.getDistanceFloat()); 
     delay(100);
 }
 
@@ -191,20 +209,27 @@ void lineFollowerTest() {
 
 void loop() {
     // Controller
-    // BP32.update();
+    BP32.update();
 
-    irSensorTest();
+    // Turn on LEDS
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+
+    // motorTest();
+
+    // irSensorTest();
+    //lineFollowerTest();
 
     // Controller Code
-    /*
+    
     for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
         GamepadPtr controller = myGamepads[i];
         if (controller && controller->isConnected()) {
             
-            Serial.println("Connected");
+            // Serial.println("Connected");
             digitalWrite(2, HIGH);
-            delay(2000);
-            digitalWrite(2, LOW);
+            // delay(2000);
+            // digitalWrite(2, LOW);
             
             // if (controller->l1() == 1) {
             //     Serial.print("Servo move");
@@ -218,19 +243,19 @@ void loop() {
             //Move forward
             if (controller->axisY() < 0) { // negative y is upward on stick
                 Serial.println(" DC motor move");
-                digitalWrite(IN1, HIGH);
-                digitalWrite(IN2, LOW);
-                digitalWrite(BIN1, LOW);
-                digitalWrite(BIN2, HIGH);
+                digitalWrite(IN1, LOW);
+                digitalWrite(IN2, HIGH);
+                digitalWrite(BIN1, HIGH);
+                digitalWrite(BIN2, LOW);
             }
 
             //Move backward
             if (controller->axisY() > 0) {
                 Serial.println(" DC motor move");
-                digitalWrite(IN1, LOW);
-                digitalWrite(IN2, HIGH);
-                digitalWrite(BIN1, HIGH);
-                digitalWrite(BIN2, LOW);
+                digitalWrite(IN1, HIGH);
+                digitalWrite(IN2, LOW);
+                digitalWrite(BIN1, LOW);
+                digitalWrite(BIN2, HIGH);
             }
 
             //Stop moving
@@ -242,31 +267,90 @@ void loop() {
                 digitalWrite(BIN2, LOW);
             }
 
-            // if (controller->axisRX() > 0) { 
-            //     Serial.println(" DC motor move");
-            //     digitalWrite(IN1, LOW);
-            //     digitalWrite(IN2, HIGH);
-            // }
-            //
-            // if (controller->axisRX() < 0) { // stop motor 1
-            //     Serial.println(" DC motor stop");
-            //     digitalWrite(IN1, LOW);
-            //     digitalWrite(IN2, LOW);
-            // }
-            //
-            // if (controller->axisRX() = 0) { // stop motor 1
-            //     Serial.println(" DC motor stop");
-            //     digitalWrite(IN1, LOW);
-            //     digitalWrite(IN2, LOW);
-            // }
+            // Turn right
+            if (controller->axisRX() > 0) { 
+                Serial.println(" DC motor move");
+                digitalWrite(IN1, LOW);
+                digitalWrite(IN2, HIGH);
+                digitalWrite(BIN1, LOW);
+                digitalWrite(BIN2, HIGH);
+            }
+            
+            // Turn left
+            if (controller->axisRX() < 0) { // stop motor 1
+                Serial.println(" DC motor stop");
+                digitalWrite(IN1, HIGH);
+                digitalWrite(IN2, LOW);
+                digitalWrite(BIN1, HIGH);
+                digitalWrite(BIN2, LOW);
+            }
+            
+            if (controller->axisRX() == 0) { // stop motor 1
+                Serial.println(" DC motor stop");
+                digitalWrite(IN1, LOW);
+                digitalWrite(IN2, LOW);
+                digitalWrite(BIN1, LOW);
+                digitalWrite(BIN2, LOW);
+            }
 
-            // PHYSICAL BUTTON A
+            // Controller Position Test
+            // Serial.print("Joystick 1 X: ");
+            // Serial.println(controller->axisX());
+            // Serial.print("Joystick 1 Y: ");
+            // Serial.println(controller->axisY());
+            // Serial.print("Joystick 2 X: ");
+            // Serial.println(controller->axisRX());
+            // Serial.print("Joystick 2 Y: ");
+            // Serial.println(controller->axisRY());
+            // Serial.println();       
+
+            // PHYSICAL BUTTONS
             if (controller->b()) {
-                Serial.println("button a pressed");
+                Serial.println("button A pressed");
+            }
+
+            if (controller->a()) {
+                Serial.println("button B pressed");
+            }
+
+            if (controller->y()) {
+                Serial.println("button X pressed");
+            }
+
+            if (controller->x()) {
+                Serial.println("button Y pressed");
+            }
+
+            if (controller->l1() == 1) {
+                Serial.println("button L1 pressed");
+                servo.write(1000);
+            }
+
+            if (controller->l1() == 0) {
+                Serial.println("button L1 pressed");
+                servo.write(1500);
+            }
+
+            if (controller->l2()) {
+                Serial.println("button L2 pressed");
+            }
+
+            if (controller->r1() == 1) {
+                Serial.println("button R1 pressed");
+                servo.write(2000);
+            }
+
+            if (controller->r1() == 0) {
+                Serial.println("button R1 pressed");
+                servo.write(1500);
+            }
+
+            if (controller->r2()) {
+                Serial.println("button R2 pressed");
             }
         }
     }
-    */
+    
     vTaskDelay(1);
 }
 
